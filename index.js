e6ebfdd59afb6e3c2f0324afe653158f44050e5d
Ramling855 <ramling855@gmail.com>
2022-07-03 13:33:25 +0530

    ff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let x = [[1,2,3],[4,5],9,8,[0]];
 
 function flatten(arr){
     let arr1 = []; 
-    
+    ///ffffffffffffffffffff
     for(let i = 0 ; i < arr.length ; i++){
         if(typeof(arr[i]) === "object" || typeof(arr[i] === "string") ){
             for(let j = 0 ; j < arr[i].length ; j++){
